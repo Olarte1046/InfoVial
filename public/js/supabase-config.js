82c0b0adac3cfb5a4858303177a0babcd3afd009
Olarte1046 <alejandroolarte1046@gmail.com>
2026-05-10 15:40:05 -0500

    fix supabase profile creation

--- public/js/supabase-config.js
+++ public/js/supabase-config.js
@@ -1,7 +1,9 @@
 const SUPABASE_URL = 'https://qrhrjtuifgukemkqwhar.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyaHJqdHVpZmd1a2Vta3F3aGFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5NDEyNDcsImV4cCI6MjA5MzUxNzI0N30.HZAbrHiKRdIjRwYmlZibXU_iJIyoqDtl0dSlY-_l4NY';
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
+
+window.supabaseClient = supabaseClient;
